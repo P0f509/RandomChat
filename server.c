6d33a6d63519f5b4c630840fd04a8f1c644ac430
P0f509 <75103084+P0f509@users.noreply.github.com>
2022-03-16 11:18:08 +0100

    Update server.c

--- server.c
+++ server.c
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
 
   printf("Starting server...\n");
 
-  const char* ip = "15.236.141.63";
+  const char* ip = "172.31.25.75";
   int port = atoi(argv[1]);
 
   pthread_t tid;
